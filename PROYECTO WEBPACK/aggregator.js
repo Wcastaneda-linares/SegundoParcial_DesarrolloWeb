63f0c96650a536cf12245ce3457b9e10d4845790
Wagner Castañeda Linares <79226276+Wcastaneda-linares@users.noreply.github.com>
2023-11-07 15:38:28 -0600

    Delete PROYECTO WEBPACK directory
    Carpeta webpack eliminada

--- PROYECTO WEBPACK/aggregator.js
+++
@@ -1,9 +0,0 @@
-// Importa modulos
-import * as calculos from './module/calculos.js';
-import * as unitConvert from './module/unitConvert.js';
-import * as passwordGen from './module/passwordGen.js';
-import * as imgViewer from './module/imgViewer.js';
-import * as taskList  from './module/taskList.js';
-
-
-
--- PROYECTO WEBPACK/js/script.js
+++
@@ -1,4 +0,0 @@
-
-    document.getElementById('botonRedireccion').addEventListener('click', function() {
-        window.location.href = '/inicio.html';
-});
